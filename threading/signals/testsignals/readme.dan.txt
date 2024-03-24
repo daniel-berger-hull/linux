95cd92e61b796f100ef35c6600aad0fab8d729b9
daniel-berger-hull <daniel.berger.hull@gmail.com>
2024-03-24 15:48:04 -0400

    Added code to the signal project, so it works correclty

--- threading/signals/testsignals/readme.dan.txt
+++ threading/signals/testsignals/readme.dan.txt
@@ -1,1 +1,12 @@
-This is a CodeBlocks project, to test the signals in Linux
+This is a CodeBlocks project, to test the signals in Linux. To test, open a Terminal and enter the following command:
+
+            kill -10 <YourPID>
+
+    Note: the '-10' is the code of SIGNUSR1.
+
+
+It is counter intuitive, to the Linux 'kill' command (used from a Terminal or a program) can be used to send signal.
+use the command 'kill -L' to see all the various types of signal that the kill command can send.
+
+
+The signal
--- threading/signals/testsignals/src/main.cpp
+++ threading/signals/testsignals/src/main.cpp
@@ -1,9 +1,49 @@
-#include <iostream>
+
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_EVENTS_TO_SIMULATE   50
+
+
+sig_atomic_t sigusr1_count = 0;
+void handler (int signal_number)
+{
+    ++sigusr1_count;
+}
+
+
+void simulateAction(int nbrEvents)
+{
+    if (nbrEvents <=0 || nbrEvents > MAX_EVENTS_TO_SIMULATE)  return;
+
+    for (int i=0;i<nbrEvents;i++)
+    {
+       printf(".\n");
+       sleep(1);
+    }
+
+
+}
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   struct sigaction sa;
+   memset (&sa, 0, sizeof (sa));
+   sa.sa_handler = &handler;
+   sigaction (SIGUSR1, &sa, NULL);
+
+   printf("Main process running at PID #%d\n", (int) getpid());
+
+   simulateAction(15);
+
+
+    printf ("SIGUSR1 was raised %d times\n", sigusr1_count);
+   printf("Terminate Main process\n");
+
     return 0;
 }
